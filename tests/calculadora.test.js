0c6af06cd3ce05c15d29b16c2a30b02c67d422b6
João Victor Martins Reis <74378259+jvmreis@users.noreply.github.com>
2025-07-08 21:15:42 +0000

    exemplo de como implementar TEST DRIVEN DEVELOPMENT

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -13,5 +13,25 @@ test("func reduzida 2", function () {
 });
 
 test("espero 1 seja 1", () => {
-  expect(2).toBe(1);
+  expect(1).toBe(1);
+});
+
+const calculator = require("../models/calculator.js");
+
+test("somar 2 + 2 deveria ser 4", () => {
+  const resultado = calculator.somar(2, 2);
+  console.log("resultado", resultado);
+  expect(resultado).toBe(4);
+});
+
+test("somar 2 + 100 deveria ser 102", () => {
+  const resultado = calculator.somar(2, 100);
+  console.log("resultado", resultado);
+  expect(resultado).toBe(102);
+});
+
+test("somar banana + 100 deveria ser 'Erro' ", () => {
+  const resultado = calculator.somar("banana", 100);
+  console.log("resultado", resultado);
+  expect(resultado).toBe("Erro");
 });
