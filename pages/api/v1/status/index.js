cf4bab69cb796bc6e4ccf075537c0b3acacd3895
João Victor Martins Reis <74378259+jvmreis@users.noreply.github.com>
2025-07-10 05:59:06 +0000

    endpoint /status implemented and fault injection studies

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,43 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log("Database query result:", result.rows);
-  res.status(200).json({ message: "API is running smoothly!" });
+  const updateAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  //const databaseName = req.query.databaseName;
+  console.log("databaseName:", databaseName);
+  const databaseOpenConnectionsResult = await database.query(
+    {
+      text: "SELECT count(*):: int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    },
+    //`SELECT count(*)::int FROM pg_stat_activity WHERE datname = '${databaseName}';`,
+    //"SELECT count(*):: int FROM pg_stat_activity WHERE datname = '" +
+    //  databaseName +
+    //  "';",
+    //"SELECT count(*):: int FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+
+  const databaseOpenConnectionsValue =
+    databaseOpenConnectionsResult.rows[0].count;
+
+  res.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        open_connections: databaseOpenConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,22 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+
+  const currentDate = new Date(responseBody.update_at).toISOString();
+  console.log("currentDate:", currentDate);
+  expect(responseBody.update_at).toEqual(currentDate);
+
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+
+  expect(responseBody.dependencies.database.open_connections).toEqual(1);
 });
+
+//test.only("teste SQL injection", async () => {
+//await fetch("http://localhost:3000/api/v1/status?databaseName=local_db"); // -- iguinora o que vem depois do -- no SQL para corrigir isso usar Query sanization
+//await fetch(
+// "http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4); --",
+//);
+//});
