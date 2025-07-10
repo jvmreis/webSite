73ba95b60dd45781629d355cb4c9a7e0dc1518af
João Victor Martins Reis <74378259+jvmreis@users.noreply.github.com>
2025-07-10 15:42:10 +0000

    make    more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -9,14 +9,22 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
+
+  console.log("postgress credentials:", {
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+  });
 
   // Using try and catch to handle connection errors
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error("Error connecting to the database:", error);
+    throw error;
   } finally {
     await client.end();
   }
